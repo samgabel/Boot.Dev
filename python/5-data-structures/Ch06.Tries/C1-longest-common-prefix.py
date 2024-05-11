e07a5631c4b103a8740c6cbc5362f448a540d8c4
Sam Gabel <sam@gabelcloud.com>
2024-05-11 15:17:28 -0700

    Update Code
    Signed-off-by: Sam Gabel <sam@gabelcloud.com>

--- python/5-data-structures/Ch06.Tries/C1-longest-common-prefix.py
+++ python/5-data-structures/Ch06.Tries/C1-longest-common-prefix.py
@@ -14,17 +14,13 @@ class Trie:
         while True:
             # we want to grab all of the children keys so long as they are not `end_symbol`
             children = []
-            # we need to store the end_symbols as well in order to determine when a word is ending
-            # this way if a word ends and we only have 1 other key, we don't continue (ie: bed, bedroom) -> words that are subsets of others
-            word_end = []
             for key in current.keys():
+                # we want to check that we don't have any words ending, bc if we do, we should not append anymore to `prefix`
                 if key == self.end_symbol:
-                    word_end.append(key)
-                else:
-                    children.append(key)
+                    break
+                children.append(key)
             # if there is only 1 key in the current level we can keep drilling down, and add the key to `prefix`
-            # also want to check that we don't have any words ending, bc if we do, we should not append anymore to `prefix`
-            if len(children) == 1 and len(word_end) < 1:
+            if len(children) == 1:
                 child = children[0]
                 prefix += child
                 current = current[child]
